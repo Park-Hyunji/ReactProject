0fc98b281d4fc14308fd6b66c61b592ae93d6be0
Park-Hyunji <phj9573@naver.com>
2023-11-18 18:07:59 +0900

    디자인수정

--- src/Youtube.js
+++ src/Youtube.js
@@ -5,8 +5,9 @@ import Header from './Layout/Header';
 const VideoPlayer = () => {
   const [playlistIndex, setPlaylistIndex] = useState(0);
   const playlist = ["_hZlQ1UdZjs", "9s9UH-2HJac", "qXHmSuZrSKE", "GVKLwh87yTA"];
+  const videoTitles = ["웹 공학", "모바일 소프트웨어", "빅데이터", "디지털콘텐츠"];
 
-  const onEndHandler = (e) => {
+  const onEndHandler = () => {
     setPlaylistIndex((prevIndex) => (prevIndex + 1) % playlist.length);
   };
 
@@ -17,17 +18,17 @@ const VideoPlayer = () => {
   return (
     <>
     <Header />
-      <div>
-        <button onClick={() => handleButtonClick(0)}>웹 공학</button>
-        <button onClick={() => handleButtonClick(1)}>모바일 소프트웨어</button>
-        <button onClick={() => handleButtonClick(2)}>빅데이터</button>
-        <button onClick={() => handleButtonClick(3)}>디지털콘텐츠</button>
-      </div>
+    <div className='youtubeName' style={{ textAlign: 'center',
+    fontSize: '30px',fontFamily: 'bori', marginTop: '20px' }}>
+      트랙 소개 영상
+    </div>
+    <div style={{ textAlign: 'center', marginTop: '20px' }}>
+      
       <YouTube
         videoId={playlist[playlistIndex]}
         opts={{
-          width: "60%",
-          height: "560px",
+          width: '60%',
+          height: '560px',
           playerVars: {
             autoplay: 1,
             modestbranding: 1,
@@ -36,6 +37,27 @@ const VideoPlayer = () => {
         }}
         onEnd={onEndHandler}
       />
+    </div>
+    <div>
+        {playlist.map((_, index) => (
+          <button
+            key={index}
+            onClick={() => handleButtonClick(index)}
+            style={{
+              padding: '10px 15px',
+              margin: '5px',
+              border: 'none',
+              borderRadius: '5px',
+              cursor: 'pointer',
+              fontSize: '16px',
+              backgroundColor: index === playlistIndex ? '#3498db' : '#ddd',
+              color: index === playlistIndex ? '#fff' : '#333',
+            }}
+          >
+            {`${videoTitles[index]}`}
+          </button>
+        ))}
+      </div>
     </>
   );
 };
